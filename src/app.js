c88112e660895ca5b8a21dfd0d164d5f2a933bb3
Ernesto Hita <ehita91@gmail.com>
2023-02-11 00:10:16 +0000

    Excuse Generator Finished

--- src/app.js
+++ src/app.js
@@ -7,5 +7,34 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+
+  let subject = [
+    "THE STUDENT",
+    "THE CAR",
+    "THE DOG",
+    "THE COUNTRY",
+    "THE HOUSE",
+    "THE COMPUTER"
+  ];
+  let adjetive = [
+    "IS BIG",
+    "IS SLOW",
+    "IS SMALL",
+    "IS INTELIGENT",
+    "IS FAST",
+    "IS CAPITALIST"
+  ];
+  let verb = ["RUN", "JUMP", "STUDING"];
+
+  function generateExcuses() {
+    document.getElementById("excuse").innerHTML =
+      subject[Math.floor(Math.random() * subject.length)] +
+      " " +
+      adjetive[Math.floor(Math.random() * adjetive.length)] +
+      " " +
+      verb[Math.floor(Math.random() * verb.length)];
+  }
+  document.getElementById("btn").addEventListener("click", generateExcuses);
+
   console.log("Hello Rigo from the console!");
 };
